3fe6d87de0fdef15f277574415154880f3a72480
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-02 06:46:38 +1000

    drawing triangle

--- src/components/triangle.jsx
+++ src/components/triangle.jsx
@@ -2,13 +2,16 @@ function Triangle() {
   return (
     <div>
       <svg
-        width="100"
-        height="100"
-        viewBox="0 0 100 100"
+        width="1000"
+        height="1000"
+        viewBox="-100 -100 200 200"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <polygon points="50,10 90,90 10,90" fill="none" stroke="black" />
-        <line x1="50" y1="10" x2="50" y2="90" stroke="red" stroke-width="2" />
+        <polygon points="-50,43 0,-43 50,43" fill="none" stroke="black" />
+        <circle cx="-40" cy="37" r="2" stroke="black" fill="black" />
+        <circle cx="0" cy="-33" r="2" stroke="black" fill="black" />
+        <circle cx="40" cy="37" r="2" stroke="black" fill="black" />
+        <circle cx="0" cy="0" r="2" stroke="black" fill="black" />
       </svg>
     </div>
   );
