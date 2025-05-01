d00314e96f96c3d1f95636fccea2714510ad29f4
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-02 07:08:19 +1000

    compose it

--- src/components/triangle.jsx
+++ src/components/triangle.jsx
@@ -7,11 +7,51 @@ function Triangle() {
         viewBox="-100 -100 200 200"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <polygon points="-50,43 0,-43 50,43" fill="none" stroke="black" />
-        <circle cx="-40" cy="37" r="2" stroke="black" fill="black" />
-        <circle cx="0" cy="-33" r="2" stroke="black" fill="black" />
-        <circle cx="40" cy="37" r="2" stroke="black" fill="black" />
-        <circle cx="0" cy="0" r="2" stroke="black" fill="black" />
+        <defs>
+          <polygon
+            id="triangle"
+            points="-50,43 0,-43 50,43"
+            fill="none"
+            stroke="black"
+          />
+          <circle
+            id="bottom-left-mark"
+            cx="-40"
+            cy="37"
+            r="2"
+            stroke="black"
+            fill="black"
+          />
+          <circle
+            id="top-mark"
+            cx="0"
+            cy="-33"
+            r="2"
+            stroke="black"
+            fill="black"
+          />
+          <circle
+            id="bottom-right-mark"
+            cx="40"
+            cy="37"
+            r="2"
+            stroke="black"
+            fill="black"
+          />
+          <circle
+            id="center-mark"
+            cx="0"
+            cy="0"
+            r="2"
+            stroke="black"
+            fill="black"
+          />
+        </defs>
+        <use href="#triangle" x="0" y="0" />
+        <use href="#bottom-left-mark" x="0" y="0" />
+        <use href="#top-mark" x="0" y="0" />
+        <use href="#bottom-right-mark" x="0" y="0" />
+        <use href="#center-mark" x="0" y="0" />
       </svg>
     </div>
   );
