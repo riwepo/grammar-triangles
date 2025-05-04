8c075ba9a7f362e4f7d460779cb0835237eb1298
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-04 17:43:58 +1000

    show the veerb skeleton

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,11 @@
-import VerbTriangle from "@/components/verb-triangle";
+import VerbSkeleton from "@/components/verb-skeleton";
 
 export default function Home() {
-  const verbForms = {
+  const bossVerb = {
     head: "eat",
     preterite: "ate",
     pastParticiple: "eaten",
     presentParticiple: "eating",
   };
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <VerbTriangle colour="green" verbForms={verbForms} />
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
-    </div>
-  );
+  return <VerbSkeleton bossVerb={bossVerb} />;
 }
