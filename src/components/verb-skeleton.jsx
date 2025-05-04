55c3045296b0d5ec470bfbfb9884305a7cedf39e
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-04 18:13:29 +1000

    put triangle in 1st cell

--- src/components/verb-skeleton.jsx
+++ src/components/verb-skeleton.jsx
@@ -3,12 +3,12 @@ import VerbTriangle from "@/components/verb-triangle";
 function VerbSkeleton({ bossVerb }) {
   return (
     <div className="grid grid-cols-3 grid-rows-3 h-screen w-full border-2 border-black overflow-hidden">
-      {/* <VerbTriangle
-        className="col-start-3 row-start-1"
+      <VerbTriangle
+        className="col-start-1 row-start-1"
         verbForms={bossVerb}
         colour="black"
       />
-      <VerbTriangle
+      {/* <VerbTriangle
         className="col-start-3 row-start-2"
         verbForms={bossVerb}
         colour="black"
@@ -18,7 +18,7 @@ function VerbSkeleton({ bossVerb }) {
         verbForms={bossVerb}
         colour="black"
       /> */}
-      <div className="border border-black col-start-1 row-start-1">11</div>
+      {/* <div className="border border-black col-start-1 row-start-1">11</div> */}
       <div className="border border-black col-start-2 row-start-1">12</div>
       <div className="border border-black col-start-3 row-start-1">13</div>
       <div className="border border-black col-start-1 row-start-2">21</div>
