c536dd6a8cd3e70d83d69a3b5b4aaddcdea40951
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-04 17:17:51 +1000

    ooreder of verb forms same as v1, v2 etc.

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,10 @@ import Triangle from "@/components/triangle";
 
 export default function Home() {
   const verbForms = {
-    head: "play",
-    presentParticiple: "playing",
-    pastParticiple: "playedxxx",
-    preterite: "played",
+    head: "eat",
+    preterite: "ate",
+    pastParticiple: "eaten",
+    presentParticiple: "eating",
   };
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
