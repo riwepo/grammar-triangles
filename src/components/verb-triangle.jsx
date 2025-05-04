630800d1a8e260f92026336f96ec890ae8fe7e8d
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-04 18:21:21 +1000

    fit better in container

--- src/components/verb-triangle.jsx
+++ src/components/verb-triangle.jsx
@@ -6,7 +6,7 @@ function VerbTriangle({ colour, verbForms, className }) {
       <svg
         width="100%"
         height="100%"
-        viewBox="-100 -100 200 200"
+        viewBox="-50 -75 150 150"
         xmlns="http://www.w3.org/2000/svg"
       >
         <defs>
