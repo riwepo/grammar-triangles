5c929bc0a86971c347dc6dae1433771ec3d9c45b
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-04 18:21:32 +1000

    add more verbs

--- src/components/verb-skeleton.jsx
+++ src/components/verb-skeleton.jsx
@@ -4,29 +4,28 @@ function VerbSkeleton({ bossVerb }) {
   return (
     <div className="grid grid-cols-3 grid-rows-3 h-screen w-full border-2 border-black overflow-hidden">
       <VerbTriangle
-        className="col-start-1 row-start-1"
-        verbForms={bossVerb}
-        colour="black"
-      />
-      {/* <VerbTriangle
-        className="col-start-3 row-start-2"
+        className="col-start-3 row-start-1 border border-black "
         verbForms={bossVerb}
         colour="black"
       />
       <VerbTriangle
-        className="col-start-3 row-start-3"
+        className="col-start-3 row-start-2 border border-black "
         verbForms={bossVerb}
         colour="black"
-      /> */}
-      {/* <div className="border border-black col-start-1 row-start-1">11</div> */}
+      />
+      <VerbTriangle
+        className="col-start-3 row-start-3 border border-black "
+        verbForms={bossVerb}
+        colour="black"
+      />
+
       <div className="border border-black col-start-2 row-start-1">12</div>
-      <div className="border border-black col-start-3 row-start-1">13</div>
+
       <div className="border border-black col-start-1 row-start-2">21</div>
       <div className="border border-black col-start-2 row-start-2">22</div>
-      <div className="border border-black col-start-3 row-start-2">23</div>
+
       <div className="border border-black col-start-1 row-start-3">31</div>
       <div className="border border-black col-start-2 row-start-3">32</div>
-      <div className="border border-black col-start-3 row-start-3">33</div>
     </div>
   );
 }
