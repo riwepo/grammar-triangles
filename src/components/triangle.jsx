ed0b34c6aa27c2933306c0010ceb048b39d33d0c
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-04 17:13:49 +1000

    implement present participle

--- src/components/triangle.jsx
+++ src/components/triangle.jsx
@@ -55,29 +55,34 @@ function Triangle({ colour, verbForms }) {
               {verbForms.pastParticiple}
             </text>
           </g>
-          <circle
-            id="center-mark"
-            cx="0"
-            cy="0"
-            r="2"
-            stroke="black"
-            fill="black"
-          />
-          <path
-            id="heart"
-            d="M0,-30
+          <g id="present-participle">
+            <path
+              transform="scale(0.1, 0.1)"
+              d="M0,-30
           C 35,-60 90,-20 0,80
           C -90,-20 -35,-60 0,-30"
-            fill="red"
-            stroke="black"
-            strokeWidth="2"
-          />
+              fill="red"
+              stroke="black"
+              strokeWidth="2"
+            />
+            <text
+              x="0"
+              y="15"
+              fill="black"
+              stroke="black"
+              fontSize="10px"
+              textAnchor="middle"
+              dominantBaseline="middle"
+            >
+              {verbForms.presentParticiple}
+            </text>
+          </g>
         </defs>
         <use href="#triangle" x="0" y="0" />
         <use href="#head" x="0" y="0" />
         <use href="#preterite" x="0" y="0" />
         <use href="#past-participle" x="0" y="0" />
-        <use href="#center-mark" x="0" y="0" />
+        <use href="#present-participle" x="0" y="0" />
         <use href="#heart" transform="scale(0.1, 0.1)" x="0" y="0" />
       </svg>
     </div>
