4eb5f3e40a1ef363bf0452d34e573c3d20009686
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-07 20:08:45 +1000

    use verb speak

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import VERBS from "@/lib/utils/verb-data";
 import { nanoid } from "nanoid";
 
 export default function Home() {
-  return <VerbSkeleton bossVerb={VERBS.eat} />;
+  return <VerbSkeleton bossVerb={VERBS.speak} />;
   /* return (
     <VerbTriangle
       uid={nanoid()}
