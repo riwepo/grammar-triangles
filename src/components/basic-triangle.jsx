eecb05557922d01edf608de8ab6a653e151ee7f4
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-08 20:13:06 +1000

    put past tenses below triangle

--- src/components/basic-triangle.jsx
+++ src/components/basic-triangle.jsx
@@ -34,14 +34,13 @@ function BasicTriangle({ uuid, colour, verbForms, formVisibility, className }) {
         <g id={`${uuid}preterite`}>
           <circle cx="-40" cy="37" r="2" stroke="black" fill="black" />
           <text
-            x="-50"
-            y="37"
+            x="-40"
+            y="52"
             fill="black"
             stroke="black"
             strokeWidth="0.5"
             fontSize="10px"
-            textAnchor="end"
-            dominantBaseline="middle"
+            textAnchor="middle"
           >
             {verbForms.preterite}
           </text>
@@ -49,14 +48,13 @@ function BasicTriangle({ uuid, colour, verbForms, formVisibility, className }) {
         <g id={`${uuid}past-participle`}>
           <circle cx="40" cy="37" r="2" stroke="black" fill="black" />
           <text
-            x="50"
-            y="37"
+            x="40"
+            y="52"
             fill="black"
             stroke="black"
             strokeWidth="0.5"
             fontSize="10px"
-            textAnchor="start"
-            dominantBaseline="middle"
+            textAnchor="middle"
           >
             {verbForms.pastParticiple}
           </text>
