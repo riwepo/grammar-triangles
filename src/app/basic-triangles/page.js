672b5aac82ccfcd651c68449df9835fd6bfe5938
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-10 10:48:42 +1000

    page title move enclosing grid to component

--- src/app/basic-triangles/page.js
+++ src/app/basic-triangles/page.js
@@ -3,26 +3,24 @@ import { VERBS } from "@/lib/utils/words";
 import { nanoid } from "nanoid";
 
 export const metadata = {
-  title: "Basic Triangle",
-  description: "Basic Triangle",
+  title: "Basic Triangles",
+  description: "Basic Triangles",
 };
 
 export default function BasicTrianglesPage() {
   return (
-    <div className="h-screen w-full overflow-hidden border-2 border-black">
-      <BasicTriangles
-        uid={nanoid()}
-        colour="black"
-        verbForms={VERBS.eat}
-        formVisibility={{
-          head: true,
-          preterite: true,
-          presentParticiple: true,
-          pastParticiple: true,
-          thirdPersonSingular: true,
-          tenseLine: true,
-        }}
-      />
-    </div>
+    <BasicTriangles
+      uid={nanoid()}
+      colour="black"
+      verbForms={VERBS.eat}
+      formVisibility={{
+        head: true,
+        preterite: true,
+        presentParticiple: true,
+        pastParticiple: true,
+        thirdPersonSingular: true,
+        tenseLine: true,
+      }}
+    />
   );
 }
