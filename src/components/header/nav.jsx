f970438f3fdcdf19a86d7cfd10e142a8e2dadc1c
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-11 11:01:06 +1000

    add links for all pages

--- src/components/header/nav.jsx
+++ src/components/header/nav.jsx
@@ -3,39 +3,50 @@
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-function Nav({ isNavOpen, onClick }) {
+function Nav() {
   const router = useRouter();
   const navLinkData = [
     { key: 1, href: `/`, name: "Home" },
 
     {
       key: 2,
-      href: `/upload`,
-      name: "Upload",
+      href: `/basic-verb-triangles`,
+      name: "Basic Verb Triangles",
     },
     {
       key: 3,
-      href: `/admin`,
-      name: "Admin",
+      href: `/basic-pronoun-triangles`,
+      name: "Basic Pronoun Triangles",
+    },
+    {
+      key: 4,
+      href: `/tenses-blueprint`,
+      name: "Tenses Blueprint",
+    },
+    {
+      key: 5,
+      href: `/modal-verbs`,
+      name: "Modal Verbs",
+    },
+    {
+      key: 6,
+      href: `/inflected-tenses`,
+      name: "Inflected Tenses",
+    },
+    {
+      key: 7,
+      href: `/inflected-tenses-not`,
+      name: "Inflected Tenses Negative",
     },
   ];
 
-  // when we are on small screen, the navbar is moved off to the side and hidden when not open
-  // note that these classes get overridden below for larger screens
-  const navOpenClosedClasses = isNavOpen
-    ? "translate-x-0 opacity-100 block pointer-events-auto"
-    : "translate-x-full opacity-0 hidden pointer-events-none";
-
   // this is used as a signal to close the mobile navigation
   const handleClick = async function (e) {
-    onClick();
     router.push(e.target.href);
   };
 
   return (
-    <nav
-      className={`${navOpenClosedClasses} absolute left-0 top-0 flex h-[100vh] w-full items-center justify-center bg-[rgba(255,255,255,0.5)] backdrop-blur transition-all duration-500 md:pointer-events-auto md:relative md:block md:h-auto md:w-auto md:translate-x-0 md:bg-transparent md:opacity-100 md:backdrop-blur-none`}
-    >
+    <nav className="absolute top-0 left-0 flex h-[100vh] w-full items-center justify-center bg-[rgba(255,255,255,0.5)] backdrop-blur transition-all duration-500 md:pointer-events-auto md:relative md:block md:h-auto md:w-auto md:translate-x-0 md:bg-transparent md:opacity-100 md:backdrop-blur-none">
       <ul className="flex list-none flex-col items-center gap-8 md:flex-row xl:gap-12">
         {navLinkData.map((link) => {
           return (
