88278757b87d55984c97957c3d1ed65719404bda
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-11 10:17:48 +1000

    use layout with header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,22 @@
 import "./globals.css";
 
-export const metadata = {
-  title: "Grammar Triangles",
-  description: "Grammar Triangles",
-};
+import { Rubik } from "next/font/google";
+
+import Header from "@/components/header";
+//import Footer from "@/components/footer";
+
+const rubik = Rubik({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="text-[0.8rem] lg:text-[0.9rem] 2xl:text-[1rem]">
+      <body
+        className={`${rubik.className} text-greys-200 grid min-h-svh grid-rows-[auto_1fr_auto]`}
+      >
+        <Header />
+        {children}
+        {/* <Footer /> */}
+      </body>
     </html>
   );
 }
