f3395988d22e8cf68b0b83dc6bddc8cbe231d2aa
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-12 07:44:44 +1000

    orgnise into folders

--- src/components/basic-verb-triangles.jsx
+++ src/components/basic-verb-triangles.jsx
@@ -6,7 +6,7 @@ import { nanoid } from "nanoid";
 
 import NormalVerbTriangle from "@/components/basic-triangles/verbs/normal-verb-triangle";
 import ToBeVerbTriangle from "@/components/basic-triangles/verbs/to-be-verb-triangle";
-import SelectDropdown from "@/components/select-dropdown";
+import SelectDropdown from "@/components/ui/select-dropdown";
 
 import { VERBS } from "@/lib/utils/words";
 
