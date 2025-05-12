22474eab47bb2dbe87da7ab65c457336a293383b
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-12 18:22:23 +1000

    make border red

--- src/components/tenses-blueprint.jsx
+++ src/components/tenses-blueprint.jsx
@@ -24,7 +24,7 @@ function TensesBlueprint({ bossVerb }) {
   }
 
   return (
-    <div className="border-red grid h-screen w-full grid-cols-[auto_1fr_1fr_1fr] grid-rows-[minmax(0,1fr)_minmax(0,0.02fr)_minmax(0,1fr)_minmax(0,0.02fr)_minmax(0,1fr)_minmax(0,0.02fr)_minmax(0,1fr)] overflow-hidden border-2">
+    <div className="border-red grid h-screen w-full grid-cols-[auto_1fr_1fr_1fr] grid-rows-[minmax(0,1fr)_minmax(0,0.02fr)_minmax(0,1fr)_minmax(0,0.02fr)_minmax(0,1fr)_minmax(0,0.02fr)_minmax(0,1fr)] overflow-hidden border-2 border-red-500">
       <p className="col-start-1 row-start-1 mx-auto content-center">Simple</p>
       <NormalVerbTriangle
         uuid={ids[0]}
