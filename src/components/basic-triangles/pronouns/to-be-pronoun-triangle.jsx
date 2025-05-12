a6fb47cef192fa6f8fe2b99b09712ff788e85dd5
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-12 18:22:59 +1000

    aspect ratio typo

--- src/components/basic-triangles/pronouns/to-be-pronoun-triangle.jsx
+++ src/components/basic-triangles/pronouns/to-be-pronoun-triangle.jsx
@@ -6,7 +6,7 @@ function ToBePronounTriangle({ uuid, colour, visibility, className }) {
       width="100%"
       height="100%"
       viewBox="-150 -55 220 110"
-      preserveAspectRatio="xMidyMid meet"
+      preserveAspectRatio="xMidYMid meet"
       //preserveAspectRatio="none"
       xmlns="http://www.w3.org/2000/svg"
       className={cn(className)}
--- src/components/basic-triangles/verbs/normal-verb-triangle.jsx
+++ src/components/basic-triangles/verbs/normal-verb-triangle.jsx
@@ -7,7 +7,7 @@ function NormalVerbTriangle({ uuid, colour, verb, visibility, className }) {
       width="100%"
       height="100%"
       viewBox="-150 -55 220 110"
-      preserveAspectRatio="xMidyMid meet"
+      preserveAspectRatio="xMidYMid meet"
       //preserveAspectRatio="none"
       xmlns="http://www.w3.org/2000/svg"
     >
