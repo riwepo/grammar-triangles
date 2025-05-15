e98b6e0647ecdea3418693469b4f5b9500d6a97f
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-15 18:16:53 +1000

    put card and heading in

--- src/components/tenses-blueprint.jsx
+++ src/components/tenses-blueprint.jsx
@@ -27,87 +27,92 @@ function TensesBlueprint({ bossVerb }) {
       onVerbSelected={handleVerbSelected}
       onLoaded={handleContainerLoaded}
     >
-      <Card className="absolute top-0 right-0 bottom-0 left-0 m-4 bg-white">
-        <div className="grid h-full grid-cols-[auto_1fr_1fr_1fr] grid-rows-[1fr_2px_1fr_2px_1fr_2px_1fr] bg-white">
-          <p className="col-start-1 row-start-1 mx-auto content-center">
-            Simple
-          </p>
-          <NormalVerbTriangle
-            uuid={svgUseIds[0]}
-            className="col-start-4 row-start-1"
-            verb={selectedVerb}
-            visibility={{ head: true }}
-            colour="black"
-          />
+      <div className="absolute top-0 right-0 bottom-0 left-0 grid grid-rows-[auto_1fr] gap-y-2">
+        <p className="row-start-1 mx-auto pt-2 text-2xl font-bold">
+          Tenses Blueprint
+        </p>
+        <Card className="absolute top-0 right-0 bottom-0 left-0 row-start-2 m-4 bg-white">
+          <div className="grid h-full grid-cols-[auto_1fr_1fr_1fr] grid-rows-[1fr_2px_1fr_2px_1fr_2px_1fr] bg-white">
+            <p className="col-start-1 row-start-1 mx-auto content-center">
+              Simple
+            </p>
+            <NormalVerbTriangle
+              uuid={svgUseIds[0]}
+              className="col-start-4 row-start-1"
+              verb={selectedVerb}
+              visibility={{ head: true }}
+              colour="black"
+            />
 
-          <div className="col-span-4 col-start-1 row-start-2 bg-gray-200"></div>
+            <div className="col-span-4 col-start-1 row-start-2 bg-gray-200"></div>
 
-          <p className="col-start-1 row-start-3 mx-auto content-center">
-            Continuous
-          </p>
-          <NormalVerbTriangle
-            uuid={svgUseIds[1]}
-            className="col-start-3 row-start-3"
-            verb={VERBS.be}
-            visibility={{ head: true }}
-            colour="blue"
-          />
-          <NormalVerbTriangle
-            uuid={svgUseIds[2]}
-            className="col-start-4 row-start-3"
-            verb={selectedVerb}
-            visibility={{ presentParticiple: true }}
-            colour="black"
-          />
+            <p className="col-start-1 row-start-3 mx-auto content-center">
+              Continuous
+            </p>
+            <NormalVerbTriangle
+              uuid={svgUseIds[1]}
+              className="col-start-3 row-start-3"
+              verb={VERBS.be}
+              visibility={{ head: true }}
+              colour="blue"
+            />
+            <NormalVerbTriangle
+              uuid={svgUseIds[2]}
+              className="col-start-4 row-start-3"
+              verb={selectedVerb}
+              visibility={{ presentParticiple: true }}
+              colour="black"
+            />
 
-          <div className="col-span-4 col-start-1 row-start-4 bg-gray-200"></div>
+            <div className="col-span-4 col-start-1 row-start-4 bg-gray-200"></div>
 
-          <p className="col-start-1 row-start-5 mx-auto content-center">
-            Perfect Simple
-          </p>
-          <NormalVerbTriangle
-            uuid={svgUseIds[3]}
-            className="col-start-2 row-start-5"
-            verb={VERBS.have}
-            visibility={{ head: true }}
-            colour="green"
-          />
-          <NormalVerbTriangle
-            uuid={svgUseIds[4]}
-            className="col-start-4 row-start-5"
-            verb={selectedVerb}
-            visibility={{ pastParticiple: true }}
-            colour="black"
-          />
+            <p className="col-start-1 row-start-5 mx-auto content-center">
+              Perfect Simple
+            </p>
+            <NormalVerbTriangle
+              uuid={svgUseIds[3]}
+              className="col-start-2 row-start-5"
+              verb={VERBS.have}
+              visibility={{ head: true }}
+              colour="green"
+            />
+            <NormalVerbTriangle
+              uuid={svgUseIds[4]}
+              className="col-start-4 row-start-5"
+              verb={selectedVerb}
+              visibility={{ pastParticiple: true }}
+              colour="black"
+            />
 
-          <div className="col-span-4 col-start-1 row-start-6 bg-gray-200"></div>
+            <div className="col-span-4 col-start-1 row-start-6 bg-gray-200"></div>
 
-          <p className="mx-autov col-start-1 row-start-7 content-center">
-            Perfect Continuous
-          </p>
-          <NormalVerbTriangle
-            uuid={svgUseIds[5]}
-            className="col-start-2 row-start-7"
-            verb={VERBS.have}
-            visibility={{ head: true }}
-            colour="green"
-          />
-          <NormalVerbTriangle
-            uuid={svgUseIds[6]}
-            className="col-start-3 row-start-7"
-            verb={VERBS.be}
-            visibility={{ pastParticiple: true }}
-            colour="blue"
-          />
-          <NormalVerbTriangle
-            uuid={svgUseIds[7]}
-            className="col-start-4 row-start-7"
-            verb={selectedVerb}
-            visibility={{ presentParticiple: true }}
-            colour="black"
-          />
-        </div>
-      </Card>
+            <p className="mx-autov col-start-1 row-start-7 content-center">
+              Perfect Continuous
+            </p>
+            <NormalVerbTriangle
+              uuid={svgUseIds[5]}
+              className="col-start-2 row-start-7"
+              verb={VERBS.have}
+              visibility={{ head: true }}
+              colour="green"
+            />
+            <NormalVerbTriangle
+              uuid={svgUseIds[6]}
+              className="col-start-3 row-start-7"
+              verb={VERBS.be}
+              visibility={{ pastParticiple: true }}
+              colour="blue"
+            />
+            <NormalVerbTriangle
+              uuid={svgUseIds[7]}
+              className="col-start-4 row-start-7"
+              verb={selectedVerb}
+              visibility={{ presentParticiple: true }}
+              colour="black"
+            />
+          </div>
+        </Card>
+      </div>
     </GrammarContainer>
   );
 }
