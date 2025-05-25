ff95ec0c9269ee5ec03c7cbb7e60b96f8a9dbba2
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-26 07:40:21 +1000

    set margin from parent

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -2,9 +2,7 @@ import { cn } from "@/lib/utils/css-class";
 
 function Card({ children, className }) {
   return (
-    <div className={cn(className, "m-4 rounded-lg p-4 shadow-md")}>
-      {children}
-    </div>
+    <div className={cn(className, "rounded-lg p-4 shadow-md")}>{children}</div>
   );
 }
 export default Card;
