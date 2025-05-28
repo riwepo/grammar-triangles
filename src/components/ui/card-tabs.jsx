e8035feaa728a90e4c8858586c9468072b2b4fcc
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-29 07:18:16 +1000

    working on tabs

--- src/components/ui/card-tabs.jsx
+++ src/components/ui/card-tabs.jsx
@@ -4,15 +4,15 @@ import { cn } from "@/lib/utils/css-class";
 
 function CardTabs({ tabs, children, className }) {
   const [selectedTabIndex, setSelectedTabIndex] = useState(0);
-  const selectedClasses = "bg-white border-b-0";
-  const unselectedClasses = "bg-gray-100 border-b";
+  const selectedClasses = "bg-white";
+  const unselectedClasses = "bg-gray-100";
   const handleClick = (index) => {
     setSelectedTabIndex(index);
   };
   return (
     <div className="flex flex-col">
       <div className={cn(className, "relative rounded-lg p-4 shadow-md")}>
-        <div className="absolute top-0 left-0 flex flex-row gap-1">
+        <div className="absolute top-0 left-0 flex w-full flex-row gap-1 border-b">
           {tabs.map((tab, index) => {
             const tabClasses =
               index === selectedTabIndex ? selectedClasses : unselectedClasses;
