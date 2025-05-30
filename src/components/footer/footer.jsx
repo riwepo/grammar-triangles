f9dd7f41517a54e6bd49b42b6ce2b0ac6cdcb566
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-31 09:44:54 +1000

    add top border

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     window.print();
   };
   return (
-    <footer className="grid h-16 w-full grid-cols-[1fr_auto] items-center justify-center px-8 py-2 print:hidden">
+    <footer className="grid h-16 w-full grid-cols-[1fr_auto] items-center justify-center border-t px-8 py-2 print:hidden">
       <button
         className="col-start-2 cursor-pointer rounded-sm border bg-gray-200 p-1 hover:bg-gray-100 focus:ring-2 focus:ring-blue-300"
         onClick={handlePrintClick}
