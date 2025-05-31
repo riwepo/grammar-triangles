1fd28d2556e3aeac3c68a0ff60d29ec751ee6846
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-05-31 12:12:01 +1000

    move print button from footer to grammar container

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,25 +1,6 @@
-"use client";
-import Image from "next/image";
-
 function Footer() {
-  const handlePrintClick = () => {
-    window.print();
-  };
   return (
-    <footer className="grid h-16 w-full grid-cols-[1fr_auto] items-center justify-center border-t px-8 py-2 print:hidden">
-      <button
-        className="col-start-2 cursor-pointer rounded-sm border bg-gray-200 p-1 hover:bg-gray-100 focus:ring-2 focus:ring-blue-300"
-        onClick={handlePrintClick}
-      >
-        <Image
-          src="/print.png"
-          alt="Button Icon"
-          width={24}
-          height={24}
-          className=""
-        />
-      </button>
-    </footer>
+    <footer className="grid h-16 w-full grid-cols-[1fr_auto] items-center justify-center border-t px-8 py-2 print:hidden"></footer>
   );
 }
 
--- src/components/grammar-container.jsx
+++ src/components/grammar-container.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import Image from "next/image";
 
 import { nanoid } from "nanoid";
 
@@ -116,6 +117,18 @@ function GrammarContainer({
       </div>
       <div className="relative flex h-screen w-full flex-col overflow-visible border bg-gray-300">
         {children}
+        <button
+          className="absolute right-2 bottom-2 cursor-pointer rounded-sm border bg-gray-200 p-1 hover:bg-gray-100 focus:ring-2 focus:ring-blue-300"
+          onClick={() => window.print()}
+        >
+          <Image
+            src="/print.png"
+            alt="Button Icon"
+            width={24}
+            height={24}
+            className=""
+          />
+        </button>
       </div>
     </>
   );
