b9ce3339db2ad8780974151098646f072e6d2931
riwepo-idioma-idiota <riwepo.au@gmail.com>
2025-06-13 17:03:22 +1000

    add pattern explanations

---
+++ src/components/pattern-explanations.jsx
@@ -0,0 +1,85 @@
+import CardTabs from "@/components/ui/card-tabs";
+import PageContent from "@/components/page-content";
+
+import { VERB_FORM_EXPLANATIONS } from "@/lib/explanation-data";
+
+function PatternExplanations() {
+  const VerbFormUse = ({ verbFormUseData, index }) => {
+    return (
+      <li>
+        <p className="mt-2 mb-2 text-xl font-bold">{`${index}. ${verbFormUseData.heading}`}</p>
+        <p className="whitespace-pre">{verbFormUseData.description}</p>
+        <p className="text-l mt-2 font-semibold">Examples</p>
+        <ul>
+          {verbFormUseData.examples.map((example) => (
+            <p
+              key={example.key}
+              className="italic"
+              dangerouslySetInnerHTML={{ __html: example.value }}
+            />
+          ))}
+        </ul>
+      </li>
+    );
+  };
+  const VerbFormExplanation = ({ verbFormData }) => {
+    return (
+      <div>
+        <p className="text-3xl font-extrabold">{verbFormData.heading}</p>
+        <p className="mt-4 mb-2 text-2xl font-extrabold">
+          How to create it from the base verb
+        </p>
+        <p className="whitespace-pre">{verbFormData.howToCreate}</p>
+        <p className="mt-2 mb-2 text-2xl font-extrabold">Creation examples</p>
+        <ul>
+          {verbFormData.creationExamples.map((example) => (
+            <li key={example.key}>{example.value}</li>
+          ))}
+        </ul>
+        <p className="mt-2 mb-2 text-2xl font-extrabold">Where it is used</p>
+        <ul>
+          {verbFormData.uses.map((use, index) => (
+            <VerbFormUse
+              key={use.key}
+              verbFormUseData={use}
+              index={index + 1}
+            ></VerbFormUse>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
+  const verbFormExplanations = [
+    <VerbFormExplanation verbFormData={VERB_FORM_EXPLANATIONS.base} />,
+    <VerbFormExplanation verbFormData={VERB_FORM_EXPLANATIONS.pastSimple} />,
+    <VerbFormExplanation
+      verbFormData={VERB_FORM_EXPLANATIONS.pastParticiple}
+    />,
+    <VerbFormExplanation
+      verbFormData={VERB_FORM_EXPLANATIONS.presentParticiple}
+    />,
+    <VerbFormExplanation
+      verbFormData={VERB_FORM_EXPLANATIONS.thirdPersonSingular}
+    />,
+  ];
+  return (
+    <div className="text-center">
+      <PageContent />
+      <CardTabs
+        tabs={[
+          VERB_FORM_EXPLANATIONS.base.heading,
+          VERB_FORM_EXPLANATIONS.pastSimple.heading,
+          VERB_FORM_EXPLANATIONS.pastParticiple.heading,
+          VERB_FORM_EXPLANATIONS.presentParticiple.heading,
+          VERB_FORM_EXPLANATIONS.thirdPersonSingular.heading,
+        ]}
+        className="mx-auto mb-4 w-3/4 bg-white"
+      >
+        {verbFormExplanations}
+      </CardTabs>
+    </div>
+  );
+}
+
+export default PatternExplanations;
--- src/components/pattern.jsx
+++ src/components/pattern.jsx
@@ -11,6 +11,7 @@ import HelperVerbBeTriangle from "@/components/triangles/helper-verb-be-triangle
 import Card from "@/components/ui/card";
 
 import { VERBS } from "@/lib/grammar-data";
+import PatternExplanations from "./pattern-explanations";
 
 function Pattern() {
   // the selected verb
@@ -27,99 +28,102 @@ function Pattern() {
   };
 
   return (
-    <GrammarContainer
-      onVerbSelected={handleVerbSelected}
-      onLoaded={handleContainerLoaded}
-    >
-      <div className="absolute top-0 right-0 bottom-0 left-0 grid grid-rows-[auto_1fr] gap-y-2">
-        <p className="row-start-1 mx-auto pt-2 text-2xl font-bold">Pattern</p>
-        <Card className="absolute top-0 right-0 bottom-0 left-0 row-start-2 m-4 bg-white">
-          <div className="grid h-full grid-cols-[auto_1fr_1fr_1fr] grid-rows-[1fr_2px_1fr_2px_1fr_2px_1fr] bg-white">
-            <p className="col-start-1 row-start-1 mx-auto content-center">
-              Simple
-            </p>
-            <BossVerbTriangle
-              uuid={svgUseIds[0]}
-              verb={selectedVerb}
-              classNames={{
-                svg: "col-start-4 row-start-1",
-                head: "visible",
-              }}
-            />
+    <>
+      <GrammarContainer
+        onVerbSelected={handleVerbSelected}
+        onLoaded={handleContainerLoaded}
+      >
+        <div className="absolute top-0 right-0 bottom-0 left-0 grid grid-rows-[auto_1fr] gap-y-2">
+          <p className="row-start-1 mx-auto pt-2 text-2xl font-bold">Pattern</p>
+          <Card className="absolute top-0 right-0 bottom-0 left-0 row-start-2 m-4 bg-white">
+            <div className="grid h-full grid-cols-[auto_1fr_1fr_1fr] grid-rows-[1fr_2px_1fr_2px_1fr_2px_1fr] bg-white">
+              <p className="col-start-1 row-start-1 mx-auto content-center">
+                Simple
+              </p>
+              <BossVerbTriangle
+                uuid={svgUseIds[0]}
+                verb={selectedVerb}
+                classNames={{
+                  svg: "col-start-4 row-start-1",
+                  head: "visible",
+                }}
+              />
 
-            <div className="col-span-4 col-start-1 row-start-2 bg-gray-200"></div>
+              <div className="col-span-4 col-start-1 row-start-2 bg-gray-200"></div>
 
-            <p className="col-start-1 row-start-3 mx-auto content-center">
-              Continuous
-            </p>
-            <HelperVerbBeTriangle
-              uuid={svgUseIds[1]}
-              classNames={{
-                svg: "col-start-3 row-start-3",
-                head: "visible",
-              }}
-            />
-            <BossVerbTriangle
-              uuid={svgUseIds[2]}
-              verb={selectedVerb}
-              classNames={{
-                svg: "col-start-4 row-start-3",
-                presentParticiple: "visible",
-              }}
-            />
+              <p className="col-start-1 row-start-3 mx-auto content-center">
+                Continuous
+              </p>
+              <HelperVerbBeTriangle
+                uuid={svgUseIds[1]}
+                classNames={{
+                  svg: "col-start-3 row-start-3",
+                  head: "visible",
+                }}
+              />
+              <BossVerbTriangle
+                uuid={svgUseIds[2]}
+                verb={selectedVerb}
+                classNames={{
+                  svg: "col-start-4 row-start-3",
+                  presentParticiple: "visible",
+                }}
+              />
 
-            <div className="col-span-4 col-start-1 row-start-4 bg-gray-200"></div>
+              <div className="col-span-4 col-start-1 row-start-4 bg-gray-200"></div>
 
-            <p className="col-start-1 row-start-5 mx-auto content-center">
-              Perfect Simple
-            </p>
-            <HelperVerbHaveTriangle
-              uuid={svgUseIds[3]}
-              classNames={{
-                svg: "col-start-2 row-start-5",
-                head: "visible",
-              }}
-            />
-            <BossVerbTriangle
-              uuid={svgUseIds[4]}
-              verb={selectedVerb}
-              classNames={{
-                svg: "col-start-4 row-start-5",
-                pastParticiple: "visible",
-              }}
-            />
+              <p className="col-start-1 row-start-5 mx-auto content-center">
+                Perfect Simple
+              </p>
+              <HelperVerbHaveTriangle
+                uuid={svgUseIds[3]}
+                classNames={{
+                  svg: "col-start-2 row-start-5",
+                  head: "visible",
+                }}
+              />
+              <BossVerbTriangle
+                uuid={svgUseIds[4]}
+                verb={selectedVerb}
+                classNames={{
+                  svg: "col-start-4 row-start-5",
+                  pastParticiple: "visible",
+                }}
+              />
 
-            <div className="col-span-4 col-start-1 row-start-6 bg-gray-200"></div>
+              <div className="col-span-4 col-start-1 row-start-6 bg-gray-200"></div>
 
-            <p className="mx-autov col-start-1 row-start-7 content-center">
-              Perfect Continuous
-            </p>
-            <HelperVerbHaveTriangle
-              uuid={svgUseIds[5]}
-              classNames={{
-                svg: "col-start-2 row-start-7",
-                head: "VISIBLE",
-              }}
-            />
-            <HelperVerbBeTriangle
-              uuid={svgUseIds[6]}
-              classNames={{
-                svg: "col-start-3 row-start-7",
-                pastParticiple: "visible",
-              }}
-            />
-            <BossVerbTriangle
-              uuid={svgUseIds[7]}
-              verb={selectedVerb}
-              classNames={{
-                svg: "col-start-4 row-start-7",
-                presentParticiple: "VISIBLE",
-              }}
-            />
-          </div>
-        </Card>
-      </div>
-    </GrammarContainer>
+              <p className="mx-autov col-start-1 row-start-7 content-center">
+                Perfect Continuous
+              </p>
+              <HelperVerbHaveTriangle
+                uuid={svgUseIds[5]}
+                classNames={{
+                  svg: "col-start-2 row-start-7",
+                  head: "VISIBLE",
+                }}
+              />
+              <HelperVerbBeTriangle
+                uuid={svgUseIds[6]}
+                classNames={{
+                  svg: "col-start-3 row-start-7",
+                  pastParticiple: "visible",
+                }}
+              />
+              <BossVerbTriangle
+                uuid={svgUseIds[7]}
+                verb={selectedVerb}
+                classNames={{
+                  svg: "col-start-4 row-start-7",
+                  presentParticiple: "VISIBLE",
+                }}
+              />
+            </div>
+          </Card>
+        </div>
+      </GrammarContainer>
+      <PatternExplanations />
+    </>
   );
 }
 
