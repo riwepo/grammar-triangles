740a9866c98b9a75f0a88a70569a7828bacb107b
riwepo <riwepo.au@gmail.com>
2025-07-07 17:29:12 +1000

    added margin top

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="grid h-16 w-full grid-cols-[1fr_auto] items-center justify-center border-t px-8 py-2 print:hidden"></footer>
+    <footer className="nt-2 grid h-16 w-full grid-cols-[1fr_auto] items-center justify-center border-t px-8 py-2 print:hidden"></footer>
   );
 }
 
