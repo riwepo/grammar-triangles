28db8a798c7d29f2cdc6aae5d53c4bc99ee55c84
riwepo <riwepo.au@gmail.com>
2025-07-08 13:02:57 +1000

    trying to get spacwr columns to collapse

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -8,15 +8,17 @@ import logo from "@/public/logo.png";
 
 function Header() {
   return (
-    <header className="flex h-16 flex-row items-center justify-between gap-4 px-8 py-0 print:hidden">
-      <Link href="/" prefetch={false} className="">
+    <header className="grid-cols-grid-cols-[auto_minmax(0,1fr)_auto_minmax(0,1fr)_auto] grid h-16 min-w-0 items-center gap-x-4 px-8 py-0 print:hidden">
+      <Link href="/" prefetch={false} className="col-start-1">
         <Image
           alt="Grammmar Triangles logo"
           src={logo}
           className="h-16 w-auto"
         />
       </Link>
-      <Nav className="mx-auto" />
+      <div className="col-start-2 min-w-0 overflow-hidden"></div>
+      <Nav className="col-start-3" />
+      <div className="col-start-4 min-w-0 overflow-hidden"></div>
     </header>
   );
 }
