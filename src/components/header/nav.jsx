79af83e1d9ba23d7690e6752277e7efc42866e0e
riwepo <riwepo.au@gmail.com>
2025-07-08 11:38:46 +1000

    change to NavLinks

--- src/components/header/nav.jsx
+++ src/components/header/nav.jsx
@@ -1,9 +1,10 @@
 "use client";
 
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 import { cn } from "@/lib/utils/css-class";
+
+import NavLink from "@/components/ui/navlink";
 
 function Nav({ className }) {
   const router = useRouter();
@@ -30,7 +31,7 @@ function Nav({ className }) {
     },
   ];
 
-  // this is used as a signal to close the mobile navigation
+  // this used to be used as a signal to close the mobile navigation
   const handleClick = async function (e) {
     router.push(e.target.href);
   };
@@ -41,14 +42,14 @@ function Nav({ className }) {
         {navLinkData.map((link) => {
           return (
             <li key={link.key}>
-              <Link
-                className="hover:text-burnt-orange-200 active:text-burnt-orange-200 text-xl font-medium focus:shadow-[0_0_0_0.5rem_rgba(230,125,34,0.5)] focus:outline-none"
+              <NavLink
                 href={link.href}
-                onClick={handleClick}
-                prefetch={false}
+                className={({ isActive }) =>
+                  `focus:shadow-orange text-xl font-medium hover:text-[#4C0000] focus:outline-none active:text-[#990000] ${isActive ? "border-b-2 border-orange-500" : ""}`
+                }
               >
                 {link.name}
-              </Link>
+              </NavLink>
             </li>
           );
         })}
