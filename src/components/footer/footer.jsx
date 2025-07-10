1c2816c1509189b203eff21e7db5c40d11e6254f
riwepo <riwepo.au@gmail.com>
2025-07-11 08:36:35 +1000

    add acknowlege link and web design link

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
+
 import logo from "@/public/logo.png";
+import WebDesignBy from "@/components/footer/web-design-by";
 
 function Footer() {
   return (
-    <footer className="grid h-24 w-full grid-cols-[auto_1fr] items-center justify-center border-t px-8 py-2 print:hidden">
+    <footer className="grid h-24 w-full grid-cols-[auto_1fr_auto_1fr_auto] items-center justify-center border-t px-8 py-2 print:hidden">
       <Link href="/" prefetch={false} className="col-start-1">
         <Image
           alt="Grammmar Triangles logo"
@@ -13,6 +15,10 @@ function Footer() {
           className="h-16 w-auto"
         />
       </Link>
+      <Link href="/acknowledgements" prefetch={false} className="col-start-3">
+        <p>Acknowledgements</p>
+      </Link>
+      <WebDesignBy className="col-start-5" />
     </footer>
   );
 }
