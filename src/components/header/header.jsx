44e7aa3c3f6619006fd1a8b3699e8b5df49aa3ae
riwepo <riwepo.au@gmail.com>
2025-07-11 09:04:44 +1000

    tweak layout

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -49,7 +49,7 @@ function Header() {
   }, [pathname]);
 
   return (
-    <header className="grid h-24 min-w-0 grid-cols-[auto_1fr_auto_1fr_auto] items-center gap-x-4 px-2 py-2 md:px-8 print:hidden">
+    <header className="grid h-24 min-w-0 grid-cols-[auto_1fr_auto_1fr_auto] items-center gap-x-4 px-4 py-2 md:justify-center md:px-8 print:hidden">
       <Link href="/" prefetch={false} className="col-start-1">
         <Image
           alt="Grammmar Triangles logo"
