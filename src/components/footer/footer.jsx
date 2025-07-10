73710c40f47bd506038031eef64d8fc1f995881b
riwepo <riwepo.au@gmail.com>
2025-07-11 08:17:23 +1000

    put logo in footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import logo from "@/public/logo.png";
+
 function Footer() {
   return (
-    <footer className="grid h-16 w-full grid-cols-[1fr_auto_1fr] items-center justify-center border-t px-8 py-2 print:hidden">
-      <p>footer</p>
+    <footer className="grid h-24 w-full grid-cols-[auto_1fr] items-center justify-center border-t px-8 py-2 print:hidden">
+      <Link href="/" prefetch={false} className="col-start-1">
+        <Image
+          alt="Grammmar Triangles logo"
+          src={logo}
+          className="h-16 w-auto"
+        />
+      </Link>
     </footer>
   );
 }
