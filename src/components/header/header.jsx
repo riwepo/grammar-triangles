ddf538de032ecd1fa86dd4e6e9daffbe88aacf16
riwepo <riwepo.au@gmail.com>
2025-07-16 18:45:09 +1000

    make reacrt first import

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,8 @@
 "use client";
 
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import React from "react";
 import { usePathname } from "next/navigation";
 
 import Nav from "@/components/header/nav";
